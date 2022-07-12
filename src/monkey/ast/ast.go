23cd90f89ad55984157e93c58fe58e9c614c6895
mizuki-n-2 <mizukizhangze@gmail.com>
2022-07-12 22:45:53 +0900

    インタプリタ完成:tada::tada::tada:

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -348,3 +348,27 @@ func (ie *IndexExpression) String() string {
 
 	return out.String()
 }
+
+type HashLiteral struct {
+	Token token.Token // '{' トークン
+	Pairs map[Expression]Expression
+}
+
+func (h *HashLiteral) expressionNode() {}
+func (h *HashLiteral) TokenLiteral() string {
+	return h.Token.Literal
+}
+func (h *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range h.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
--- src/monkey/evaluator/builtins.go
+++ src/monkey/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "github.com/mizuki-n-2/go_interpreter/src/monkey/object"
+import (
+	"fmt"
+	"github.com/mizuki-n-2/go_interpreter/src/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -95,6 +98,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
